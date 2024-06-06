49df3e3a37b075fc2bdc42e9b0449b94a50630b9
ivan-meling-luxoft <IvGarciaTopeteMeling@luxoft.com>
2024-06-06 12:18:45 -0700

    Initial commit for FooBar exercise

---
+++ CodeExercise.Implementation/FooBar.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeExercise.Implementation;
+
+public class FooBar
+{
+    /// <summary>
+    /// Returns the numbers from <paramref name="start"/> to <paramref name="finish"/> with the exception of returning "foo" instead of multiples of 3, "bar" for multiples of 5 and "foobar" for multiples of both 3 and 5
+    /// </summary>
+    /// <param name="start">Lower inclusive limit, should be greater than zero and lower than <paramref name="finish"/></param>
+    /// <param name="finish">Upper inclusive limit, should be greater than <paramref name="start"/></param>
+    /// <returns></returns>
+    public IEnumerable<string> CalculateFooBar(int start, int finish)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ CodeExercise.Test/FooBarTests.cs
@@ -0,0 +1,176 @@
+﻿using CodeExercise.Implementation;
+
+namespace CodeExercise.Test;
+
+public class FooBarTests
+{
+    [Fact]
+    public void ShouldFail_InvalidParameter_LowerThanOrEqualsZero()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new FooBar().CalculateFooBar(-1, 1));
+        Assert.Throws<ArgumentOutOfRangeException>(() => new FooBar().CalculateFooBar(0, 1));
+    }
+
+    [Fact]
+    public void ShouldFail_InvalidParameter_GreaterThanOrEqualsFinish()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new FooBar().CalculateFooBar(1, 1));
+        Assert.Throws<ArgumentOutOfRangeException>(() => new FooBar().CalculateFooBar(2, 1));
+    }
+
+    [Fact]
+    public void ShouldPass_Result_OneToTwentyFive()
+    {
+        var fooBar = new FooBar();
+        var start = 1;
+        var finish = 25;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_OneToFifty()
+    {
+        var fooBar = new FooBar();
+        var start = 1;
+        var finish = 50;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_OneToSeventyFive()
+    {
+        var fooBar = new FooBar();
+        var start = 1;
+        var finish = 75;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_OneToOneHundred()
+    {
+        var fooBar = new FooBar();
+        var start = 1;
+        var finish = 100;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_TwoToTwentyFive()
+    {
+        var fooBar = new FooBar();
+        var start = 2;
+        var finish = 25;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_TwoToFifty()
+    {
+        var fooBar = new FooBar();
+        var start = 2;
+        var finish = 50;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_TwoToSeventyFive()
+    {
+        var fooBar = new FooBar();
+        var start = 2;
+        var finish = 75;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_TwpToOneHundred()
+    {
+        var fooBar = new FooBar();
+        var start = 2;
+        var finish = 100;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_ThreeToTwentyFive()
+    {
+        var fooBar = new FooBar();
+        var start = 3;
+        var finish = 25;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_ThreeToFifty()
+    {
+        var fooBar = new FooBar();
+        var start = 3;
+        var finish = 50;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_ThreeToSeventyFive()
+    {
+        var fooBar = new FooBar();
+        var start = 3;
+        var finish = 75;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [Fact]
+    public void ShouldPass_Result_ThreeToOneHundred()
+    {
+        var fooBar = new FooBar();
+        var start = 3;
+        var finish = 100;
+        var expectedResult = FooBarMock.FooBar[(start - 1)..finish];
+
+        var actualResult = fooBar.CalculateFooBar(start, finish);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
